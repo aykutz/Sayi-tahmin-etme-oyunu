4f1e99c225dddb3502e6c98f96b0f1bf84181f62
Hüseyin Aykut <75700979+aykutz@users.noreply.github.com>
2021-07-25 20:52:20 +0300

    Add files via upload

---
+++ sayı tahmin etme.py
@@ -0,0 +1,47 @@
+import random
+import time
+
+print("""*************************************************************
+
+SAYI TAHMİN OYUNU
+
+1 ile 50 arasındaki sayıyı tahmin edin.
+
+Tahmin için 5 hakkınız vardır !!!!!
+
+*************************************************************
+""")
+
+rastgele_sayı = random.randint(1,50)
+tahmin_hakkı = 5
+
+while True:
+
+    tahmin = int(input("tahmininiz: "))
+
+
+    if(tahmin < rastgele_sayı):
+        print("tahmininiz kontrol ediliyor...")
+        time.sleep(1)
+
+        print("daha yüksek bir sayı girin")
+        tahmin_hakkı -=1
+        print("tahmin hakkınız: ",tahmin_hakkı)
+
+    elif(tahmin > rastgele_sayı):
+        print("tahmininiz kontrol ediliyor...")
+        time.sleep(1)
+
+        print("daha küçük bir sayı girin")
+        tahmin_hakkı -= 1
+        print("tahmin hakkınız: ", tahmin_hakkı)
+
+    else:
+        print("tahmininiz kontrol ediliyor...")
+        time.sleep(1)
+        print("TEBRİKLER! saymımız: ",rastgele_sayı)
+        break
+    if(tahmin_hakkı == 0):
+        print("üzgünüm bilemedin tahmin hakkın kalmadı")
+        print("sayımız: ",rastgele_sayı)
+        break
